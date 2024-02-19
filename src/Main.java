a579a45ddb5ab54751ebc1af7788400036a400cf
NStriebel <StriebelNJ21@gcc.edu>
2024-02-19 14:36:53 -0500

    started singing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ public class Main {
     public static void main(String[] args){
         System.out.println("These are words on the console.");
         System.out.println("These are more words.");
+
         System.out.println("Blessed be the Lord our God of Israel");
+        System.out.println("From everlasting");
+        System.out.println("To everlasting");
+        System.out.println("Blessed be the Lord our God of Israel");
+        System.out.println("From everlasting");
+        System.out.println("To everlasting");
+
+        System.out.println("Let all the Romans say amen");
+        System.out.println("Let all the Galatians say amen");
+        System.out.println("Let all the Corinthians say amen");
+        System.out.println("Praise the Lord!");
     }
 }
