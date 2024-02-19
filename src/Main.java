7dc0fac2e918302ebe2aa6cabf4d0a393b4f060c
NStriebel <StriebelNJ21@gcc.edu>
2024-02-19 14:29:59 -0500

    started to sing

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("These are words on the console.");
         System.out.println("These are more words.");
+        System.out.println("Blessed be the Lord our God of Israel");
     }
 }
